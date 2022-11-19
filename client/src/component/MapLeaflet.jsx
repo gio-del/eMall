87d00f57dd14febf11f622896fae3b09e906ac7d
lorenzo-battiston <lorenzo1.battiston@mail.polimi.it>
2022-11-19 22:42:51 +0100

    SearchBar

--- client/src/component/MapLeaflet.jsx
+++ client/src/component/MapLeaflet.jsx
@@ -1,25 +1,36 @@
 import { MapContainer, TileLayer,Marker, Popup, useMap} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import SearchBar from './SearchBar';
 
 export default function MapLeaflet () {
     return (
-        <div className='flex-grow'>
-            <MapContainer 
-                    style={{ height: '90vh', width: '100wh' }}
-                    center={[41.87, 12.56]} 
-                    zoom={6} 
-                    scrollWheelZoom={true} 
-                    >
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[41.87, 12.56]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>
+        <div>
+             <div>
+                <div class="w-full h-screen bg-gray-200 flex justify-center items-center">
+                    <div class="bg-gray-400 w-full h-full relative z-0">
+                        <MapContainer
+                            style={{ height: '100vh'}}
+                            center={[41.87, 12.56]}
+                            zoom={6}
+                            scrollWheelZoom={true}
+                        >
+                            <TileLayer
+                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            <Marker position={[41.87, 12.56]}>
+                                <Popup>
+                                    A pretty CSS3 popup. <br /> Easily customizable.
+                                </Popup>
+                            </Marker>
+                        </MapContainer>
+                        <div class="absolute inset-x-0 top-4 items-center max-w-md mx-auto z-10">
+                            <SearchBar/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         </div>
         
     )
