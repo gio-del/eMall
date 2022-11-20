478dbc8e0c926acb8642fbec14cea45dfb907a38
gio-del <giovanni.delucia@polimi.it>
2022-11-20 11:14:56 +0100

    mobile navbar and map refactoring

--- client/src/component/MapLeaflet.jsx
+++ client/src/component/MapLeaflet.jsx
@@ -9,39 +9,36 @@ export default function MapLeaflet() {
   const [longitude, setLongitude] = useState(0)
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      setLatitude(position.coords.latitude)
-      setLongitude(position.coords.longitude)
-    })
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLatitude(position.coords.latitude)
+        setLongitude(position.coords.longitude)
+      },
+      (error) => {
+        //alert(error.message)
+      },
+    )
   }, [])
 
   return (
-    <div>
-      <div>
-        <div class="w-full h-screen bg-gray-200 flex justify-center items-center">
-          <div class="bg-gray-400 w-full h-full relative z-0">
-            <MapContainer
-              style={{ height: '100vh' }}
-              center={[41.87, 12.56]}
-              zoom={6}
-              scrollWheelZoom={true}
-            >
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              <Marker position={[latitude, longitude]}>
-                <Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-              </Marker>
-            </MapContainer>
-            <div class="absolute inset-x-0 top-4 items-center max-w-md mx-auto z-10">
-              <SearchBar />
-            </div>
-          </div>
+    <>
+      <MapContainer
+        style={{ height: 'calc(100vh - 4.5rem)', width: '100vw' }}
+        center={[41.87, 12.56]}
+        zoom={6}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[latitude, longitude]}>
+          <Popup>{latitude + ' ' + longitude}</Popup>
+        </Marker>
+        <div className="absolute inset-x-0 top-4 items-center max-w-md mx-auto z-10">
+          <SearchBar />
         </div>
-      </div>
-    </div>
+      </MapContainer>
+    </>
   )
 }
