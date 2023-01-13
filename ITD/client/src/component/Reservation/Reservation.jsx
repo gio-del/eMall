c087bd56434266eb18cae4db170e3344369f7eff
lorenzo-battiston <lorenzo1.battiston@mail.polimi.it>
2023-01-13 14:01:15 +0100

    reservation

--- ITD/client/src/component/Reservation/Reservation.jsx
+++ ITD/client/src/component/Reservation/Reservation.jsx
@@ -1,9 +1,22 @@
+import useWindowDimensions from "../Car/useWindowDimensions";
+import SingleReservation from "./SingleReservation";
+
 export default function Reservation (){
+    const { height, width } = useWindowDimensions();
+
     return <>
-    <div className="flex items-center justify-center">
-        <p className="font-medium text-2xl">My Reservation</p>
-        <div className="flex">
-            <div className=""></div>
+    <div className="flex flex-col"
+                style= {{height : `calc(${height}px - 3.5rem`}}>
+        <div className="flex items-center justify-center">
+            <p className="font-medium text-2xl dark:text-tertiary text-dk-secondary">My Reservation</p>
+
+        </div>
+        <div className="flex  w-full">
+            <div className="grid grid-flow-row grid-cols-1 md:grid-cols-3 gap-x-4 w-full overflow-hidden mt-4">
+                <SingleReservation/>
+                <SingleReservation/>
+                <SingleReservation/>
+            </div>
         </div>
     </div>
     </>
--- ITD/client/src/component/Reservation/SingleReservation.jsx
+++ ITD/client/src/component/Reservation/SingleReservation.jsx
@@ -1,3 +1,45 @@
+import ConnectorSVG from "../utilitycomponent/ConnectorSVG";
+
 export default function SingleReservation (){
-    return <></>
+    return <>
+        <div className="flex flex-col border-2 dark:border-tertiary rounded-2xl">
+            <div className="flex justify-between m-2">
+                <div className="flex flex-col">
+                    <p className="text-tertiary">EVCP1</p>
+                    <p className="text-tertiary">Via Gran Sasso, 1, Milano</p>
+                </div>
+                <div className="flex bg-dk-primary rounded-xl">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="48"
+                        width="48"
+                        className="fill-dk-secondary dark:fill-tertiary"
+                        viewBox="0 0 48 48"
+                    > <path d="M16 30h3v-6.5h9.2v4.25L34 22l-5.8-5.8v4.3H17.5q-.65 0-1.075.425Q16 21.35 16 22Zm8 14.15q-.6 0-1.175-.2-.575-.2-.975-.6l-17.2-17.2q-.4-.4-.6-.975-.2-.575-.2-1.175 0-.6.2-1.175.2-.575.6-.975l17.2-17.2q.4-.4.975-.6.575-.2 1.175-.2.6 0 1.175.2.575.2.975.6l17.2 17.2q.4.4.6.975.2.575.2 1.175 0 .6-.2 1.175-.2.575-.6.975l-17.2 17.2q-.4.4-.975.6-.575.2-1.175.2Z" />
+                    </svg>
+                </div>
+            </div>
+            <div className="flex justify-between m-2">
+                <div className="flex flex-col">
+                    <p className="text-tertiary">From</p>
+                    <p className="text-tertiary">14.30</p>
+                </div>
+                <div className="flex flex-col">
+                    <p className="text-tertiary text-right">To</p>
+                    <p className="text-tertiary">16.30</p>
+                </div>
+            </div>
+            <div className="flex justify-between m-2">
+                <div className="flex flex-col">
+                    <p className="text-tertiary">From</p>
+                    <p className="text-tertiary">14.30</p>
+                </div>
+          
+                <div className="flex flex-col">
+                    <p className="text-tertiary text-right">To</p>
+                    <p className="text-tertiary">16.30</p>
+                </div>
+            </div>
+        </div>
+    </>
 }
