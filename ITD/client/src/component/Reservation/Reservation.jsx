570db5af89668cf2b008ff1f1fa56bf6c2f64330
lorenzo-battiston <lorenzo1.battiston@mail.polimi.it>
2023-01-15 11:43:06 +0100

    reservation completed

--- ITD/client/src/component/Reservation/Reservation.jsx
+++ ITD/client/src/component/Reservation/Reservation.jsx
@@ -1,40 +1,80 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import useWindowDimensions from "../Car/useWindowDimensions";
+import TabSelector from "../utilitycomponent/tabSelector";
 import SingleReservation from "./SingleReservation";
 
+const testUpcomingReservation = {
+    'date' : '23 January 2023',
+    'evcp' : 'evcp1',
+    'address' : 'Via Gran Sasso 1, Milano',
+    'start_time' : '13.00',
+    'end_time' : '16.00',
+    'connector' : 'CCS2',
+    'power' : '100kW',
+    'cost' : null,
+    'kw_charged' : null,
+}
+
+const testPastReservation = {
+    'date' : '01 March 2020',
+    'evcp' : 'evcp1',
+    'address' : 'Via Gran Sasso 1, Milano',
+    'start_time' : '13.00',
+    'end_time' : '16.00',
+    'connector' : 'CCS2',
+    'power' : '100kW',
+    'cost' : '52,43€',
+    'kw_charged' : '65.4'
+}
+
 export default function Reservation (){
-    const { height, width } = useWindowDimensions();
+    const {height, width} = useWindowDimensions()
+    const [currentTab, setCurrentTab] = useState('Upcoming');
+
+    const handleTabChange = tab => {
+        setCurrentTab(tab);
+    };
+
+    useEffect(() => {
+        if (currentTab === 'Upcoming') {
+            const upcomingReservations = document.querySelectorAll('.Upcoming');
+            upcomingReservations.forEach(reservation => reservation.classList.remove('hidden'));
+            const pastReservations = document.querySelectorAll('.Past');
+            pastReservations.forEach(reservation => reservation.classList.add('hidden'));
+        } else {
+            const pastReservations = document.querySelectorAll('.Past');
+            pastReservations.forEach(reservation => reservation.classList.remove('hidden'));
+            const upcomingReservations = document.querySelectorAll('.Upcoming');
+            upcomingReservations.forEach(reservation => reservation.classList.add('hidden'));
+        }
+    }, [currentTab])
 
     return <>
-    <div className="flex flex-col"
-                style= {{height : `calc(${height}px - 3.5rem`}}>
-        <div className="flex items-center justify-center mt-8">
+    <div className="flex flex-col">
+        <div className="flex items-center justify-center m-4 md:m-8">
             <p className="font-medium text-2xl dark:text-tertiary text-dk-secondary">My Reservation</p>
 
         </div>
-        <div className="flex justify-center">
-            <ul className="mt-6 select-none cursor-pointer inline-flex justify-around w-full md:w-4/5 lg:w-3/5 dark:text-tertiary text-dk-secondary font-light border-b-2 text-center dark:border-b-tertiary border-b-dk-secondary">
-                <li
-                    id="tab-0"
-                    className="mx-2 border-b-0 dark:border-b-tertiary border-b-dk-secondary w-1/5 hover:border-b-2 hover:border-b-searchInput dark:hover:border-b-tertiary"
-                >
-                    Past
-                </li>
-                <li
-                    id="tab-1"
-                    className="mx-2 border-b-2 dark:border-b-tertiary border-b-dk-secondary w-1/5 hover:border-b-2 hover:border-b-searchInput dark:hover:border-b-tertiary font-bold "
-                >
-                    Upcoming
-                </li>
-            </ul>
-        </div>
-        
-        <div className="flex w-full justify-center mt-4">
-            <div className="grid grid-flow-row grid-cols-1 md:grid-cols-3 gap-4 w-full md:w-4/5 lg:w-3/5 overflow-hidden m-2">
-                <SingleReservation />
-                <SingleReservation />
-                <SingleReservation />
+        <div className="max-sm:flex-col md:flex  md:h-full">
+            <TabSelector tabs={['Upcoming', 'Past']} onTabChange={handleTabChange}/>
+            <div className="flex w-full justify-center h-min">
+                <div className="grid grid-flow-row grid-cols-1 md:grid-cols-3 gap-4 w-full overflow-hidden m-6">
+                    
+                    <SingleReservation reservation={testUpcomingReservation}/>
+                    <SingleReservation reservation={testUpcomingReservation}/>
+                    <SingleReservation reservation={testUpcomingReservation}/>
+                    <SingleReservation reservation={testUpcomingReservation}/>
+                    <SingleReservation reservation={testUpcomingReservation}/>
+                    <SingleReservation reservation={testPastReservation}/>
+                    <SingleReservation reservation={testPastReservation}/>
+                    <SingleReservation reservation={testPastReservation}/>
+                    <SingleReservation reservation={testPastReservation}/>
+                    
+                </div>
             </div>
         </div>
+        
     </div>
     </>
 }
--- ITD/client/src/component/Reservation/SingleReservation.jsx
+++ ITD/client/src/component/Reservation/SingleReservation.jsx
@@ -1,52 +1,66 @@
 import ConnectorSVG from "../utilitycomponent/ConnectorSVG";
 
-export default function SingleReservation (){
+
+export default function SingleReservation ({reservation}){
     return <>
-        <div className="flex flex-col border-2 dark:border-searchInput rounded-3xl py-2 px-4">
-            <div className="flex justify-between my-2">
-                <div className="flex flex-col">
-                    <p className="text-tertiary text-md font-medium">EVCP1</p>
-                    <p className="text-tertiary text-sm">Via Gran Sasso, 1, Milano</p>
-                </div>
-                <div className="flex">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="48"
-                        width="48"
-                        className="fill-dk-secondary dark:fill-tertiary"
-                        viewBox="-24 -12 72 72"
-                    > <path d="M16 30h3v-6.5h9.2v4.25L34 22l-5.8-5.8v4.3H17.5q-.65 0-1.075.425Q16 21.35 16 22Zm8 14.15q-.6 0-1.175-.2-.575-.2-.975-.6l-17.2-17.2q-.4-.4-.6-.975-.2-.575-.2-1.175 0-.6.2-1.175.2-.575.6-.975l17.2-17.2q.4-.4.975-.6.575-.2 1.175-.2.6 0 1.175.2.575.2.975.6l17.2 17.2q.4.4.6.975.2.575.2 1.175 0 .6-.2 1.175-.2.575-.6.975l-17.2 17.2q-.4.4-.975.6-.575.2-1.175.2Z" />
-                    </svg>
-                </div>
+        <div className={`flex flex-col ${reservation.cost == null ? "Upcoming" : "Past"}`}>
+            <div className="flex justify-center">
+                <p className="dark:text-tertiary font-medium italic text">{reservation.date}</p>
             </div>
-            <div className="flex justify-between my-2">
-                <div className="flex flex-col">
-                    <p className="text-tertiary text-sm">From</p>
-                    <p className="text-tertiary text-md font-medium">14.30</p>
+            <div className="flex flex-col border-2 dark:border-searchInput rounded-3xl py-2 px-4">
+                <div className="flex justify-between my-2">
+                    <div className="flex flex-col">
+                        <p className="text-tertiary text-md font-medium">{reservation.evcp}</p>
+                        <p className="text-tertiary text-sm">{reservation.address}</p>
+                    </div>
+                    <div className="flex">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="48"
+                            width="48"
+                            className="fill-dk-secondary dark:fill-tertiary"
+                            viewBox="-24 -12 72 72"
+                        > <path d="M16 30h3v-6.5h9.2v4.25L34 22l-5.8-5.8v4.3H17.5q-.65 0-1.075.425Q16 21.35 16 22Zm8 14.15q-.6 0-1.175-.2-.575-.2-.975-.6l-17.2-17.2q-.4-.4-.6-.975-.2-.575-.2-1.175 0-.6.2-1.175.2-.575.6-.975l17.2-17.2q.4-.4.975-.6.575-.2 1.175-.2.6 0 1.175.2.575.2.975.6l17.2 17.2q.4.4.6.975.2.575.2 1.175 0 .6-.2 1.175-.2.575-.6.975l-17.2 17.2q-.4.4-.975.6-.575.2-1.175.2Z" />
+                        </svg>
+                    </div>
                 </div>
-                <div className="border-b-2 border-white"></div>
-                <div className="flex flex-col">
-                    <p className="text-tertiary text-right text-sm">To</p>
-                    <p className="text-tertiary text-right text-md font-medium">16.30</p>
+                <div className="flex justify-between my-2">
+                    <div className="flex flex-col">
+                        <p className="text-tertiary text-sm">From</p>
+                        <p className="text-tertiary text-md font-medium">{reservation.start_time}</p>
+                    </div>
+                    <div className="border-b-2 border-white"></div>
+                    <div className="flex flex-col">
+                        <p className="text-tertiary text-right text-sm">To</p>
+                        <p className="text-tertiary text-right text-md font-medium">{reservation.end_time}</p>
+                    </div>
                 </div>
+                <div className="flex justify-between my-2">
+                    <div className="flex flex-col">
+                        <p className="text-tertiary text-sm">Connector</p>
+                        <p className="text-tertiary text-md font-medium">CCS 2</p>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <p className="text-tertiary text-right text-sm">Max Output</p>
+                        <p className="text-tertiary text-right text-md font-medium">{reservation.connector}</p>
+                    </div>
+                </div>
+                <div className="flex items-center justify-center">
+                    {reservation.cost == null ? (
+                        <div className="flex bg-dk-primary rounded-xl items-center justify-center w-1/2 py-2">
+                            <p className="text-tertiary font-medium">Start</p>
+                        </div>
+                    ) : (
+                        <div className="flex justify-center w-1/2 py-2">
+                            <p className="text-tertiary font-medium middle text-center text-sm">Costs: ${reservation.cost} for ${reservation.kw_charged} kWh</p>
+                        </div>
+                    )}
+                </div>
+
+
             </div>
-            <div className="flex justify-between my-2">
-                <div className="flex flex-col">
-                    <p className="text-tertiary text-sm">Connector</p>
-                    <p className="text-tertiary text-md font-medium">CCS 2</p>
-                </div>
-             
-                <div className="flex flex-col">
-                    <p className="text-tertiary text-right text-sm">Max Output</p>
-                    <p className="text-tertiary text-right text-md font-medium">110kW</p>
-                </div>
-            </div>
-            <div className="flex items-center justify-center">
-                <div className="flex bg-dk-primary rounded-xl items-center justify-center w-1/2 py-2">
-                    <p className="text-tertiary font-medium">Start</p>
-                </div>
-            </div>
-            
         </div>
+        
     </>
 }
