2e0a7cf4ef8c3b4baf3438381d9da88c9f68b3f9
lorenzo-battiston <lorenzo1.battiston@mail.polimi.it>
2023-01-24 15:21:47 +0100

    selector tab dash

--- ITD/client/src/component/Dashboard/EnergyTab.jsx
+++ ITD/client/src/component/Dashboard/EnergyTab.jsx
@@ -1,3 +1,14 @@
-export default function EnergyTab() {
-  return 'energy'
+import { useState } from "react";
+import TabSelectorDash from "../utilitycomponent/TabSelectorDash";
+
+export default function EnergyTab({evcpList}) {
+  const [currentEvcp, setCurrentEvcp] = useState('evcp1')
+
+  return <>
+  <TabSelectorDash 
+    tabs={evcpList}
+    currentTab={currentEvcp}
+    setCurrentTab={setCurrentEvcp}
+  />
+  </>
 }
