8a0dfbab1936cd9c90b959cc34f56cc1aba234a8
gio-del <giovanni.delucia@polimi.it>
2023-02-02 09:13:33 +0100

    minor fix Registration View

--- ITD/client/src/view/BeforeLoginView.jsx
+++ ITD/client/src/view/BeforeLoginView.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from 'react'
-import { Outlet, useNavigate, useRoutes } from 'react-router-dom'
+import { useRoutes } from 'react-router-dom'
 import NavBar from '../component/NavBar'
 import LoginView from './LoginView'
 import RegistrationView from './RegistrationView'
@@ -8,13 +7,6 @@ import WelcomeView from './WelcomeView'
  * This view should handle the token authentication, if there's a token redirect to the main content
  */
 export default function BeforeLoginView() {
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    if (false)
-      //this should be if (there is a token) so no reason to go to the landing page, and navigate to the main content
-      navigate('/main')
-  }, [])
   const activeRoutes = useRoutes([
     {
       path: '/',
@@ -29,6 +21,7 @@ export default function BeforeLoginView() {
       element: <RegistrationView />,
     },
   ])
+
   return (
     <>
       <NavBar />
